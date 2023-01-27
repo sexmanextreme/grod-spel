94f570523167cdc02891cb22a117d0419177ce5a
Saz <jesrisak@gmail.com>
2023-01-27 19:36:27 +0100

    Initial commit

---
+++ kombat.java
@@ -0,0 +1,91 @@
+package spel;
+import java.util.Scanner;
+public class kombat {
+
+
+    static void slag(String [] args){
+       spelet feet = new spelet();
+       Scanner sex1=new Scanner(System.in); 
+       boolean tur;
+    //kombat startar
+    //check för att se om både du och fienden lever
+    if(feet.fien[0]>0&& feet.s_HP>0){
+        atk();
+        System.out.println("du attackerar!");
+//val för det du ska göra
+do{ System.out.println("1. offence \n 2. defence \n 3. magi"); int antsex=sex1.nextInt();
+switch (antsex){
+            case 1 -> {int cool=(int) Math.floor(Math.random() * 10);
+                //gör skada du vet, ju högre, ju mer skada desto lägre chans att träffa
+                atk();
+                System.out.println("1. HÅRT \n 2. HÅRDARE\n 3. HÅRDAST");
+                int allsex1=sex1.nextInt();
+                switch(allsex1){
+                    case 1 ->{
+                        
+                        
+                    }
+                    case 2 ->{
+                        
+                        
+                    }
+                    case 3 ->{
+                        
+                        
+                    }
+                }
+            }
+            case 2 -> {// ökar bara defence mellan två tal i en runda
+ 
+            }
+            case 3 -> {//utility attacker
+                System.out.println("1. HÅRT\n2. HÅRDARE");
+                int allsex2=sex1.nextInt();
+                switch(allsex2){
+                    case 1 ->{
+                        
+                        
+                    }
+                    case 2 ->{
+                        
+                        
+                    }
+                }
+            }
+            default -> {System.out.println("välj en annan");
+            
+            
+            }
+            
+            
+        }  tur=false;
+}while(tur);
+    }else if(feet.s_HP>0&&feet.fien[0]<=0){//fienden dör du lever
+        System.out.println("du vann!    woo");
+}
+    else if(feet.s_HP<=0&&feet.fien[0]>0){//du dör fienden lever
+        System.out.println("""
+                           sucks to suck
+                            ass to ass
+                           """);
+}
+    
+    
+}
+    static void atk(){
+System.out.print
+        
+("""
+ |-----------------------------------------------------------------------------------|
+ |                                                                                   |
+ |                                                                                   |
+ |                                      (0)-(0)                                      |
+ |                                     .-(___)-.                                     |
+ |                                      _<   >_                                      |
+ |                                      \\/   \\/                                      |
+ |                                                                                   |
+ |                                                                                   |
+ |                                                                                   |
+ |-----------------------------------------------------------------------------------|
+ """);
+}}
---
+++ spelet.java
@@ -0,0 +1,79 @@
+package spel;
+
+import java.util.Arrays;
+import java.io.*;
+import java.util.Scanner;
+public class spelet {
+    kombat slag = new kombat();
+            
+        int s_HP=100; int s_ATKs=15; int s_ATKm=33; int s_ATKh=45; int s_DEF=10; int s_MAN=8; int s_SPD=10;
+        int[] s_atk={s_ATKs,s_ATKm,s_ATKh};
+        int[] fien={100,15,30,45,10};
+        int[] fiende0=new int[5];
+        int[] fiende1=new int[5];
+        int[] fiende2=new int[5];
+        int[] fiende3=new int[5];
+    public static void main(String[] args){
+        spelet sexig = new spelet();
+        
+
+        //int[] spelare={s_HP,s_atk,s_DEF,s_MAN,s_SPD};
+  
+        Scanner sex=new Scanner(System.in); 
+        String ANSI_GREEN = "\u001B[32m";
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_ORANGE = "\u001B[33m";
+        System.out.println(ANSI_GREEN +"""
+                         Gröna grodan hoppar dig i famnen
+                        
+                                       _   _
+	                              (.)_(.)
+	                           _ (   _   ) _
+	                          / \\/`-----'\\/ \\
+	                        __\\ ( (     ) ) /__
+	                        )   /\\ \\._./ /\\   (
+	                         )_/ /|\\   /|\\ \\_(
+                          tryck på enter för att fortsätta"""+ANSI_RESET);
+        sex.nextLine();
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("vi ber å tappra äventyrare vad är ert namn"+ ANSI_ORANGE);
+        String namn=sex.nextLine();
+        System.out.println("tappre "+namn +" ge oss er längd och vikt!");
+        int lngd=sex.nextInt();
+        int vikt=sex.nextInt();
+        /*String kropp;
+        int[] t_trait={50,0,0,0,0,0,-5};
+        int[] strait={-20,0,0,0,0,0,10};
+        if(lngd>vikt) { kropp="smaling";
+        s_HP+=t_trait[0]; s_SPD+=t_trait[6];
+        } else { kropp="tjock";
+        s_HP+=strait[0]; s_SPD+=strait[6];
+        }
+        
+        System.out.println("din kroppstyp har automatiskt satts som "+ kropp);
+        */
+        
+        
+        //System.out.println(namn+" vad är din klass?!");
+        kombat.slag(args);
+    
+    }
+static void atk(){
+System.out.print
+        
+("""
+ |-----------------------------------------------------------------------------------|
+ |                                                                                   |
+ |                                                                                   |
+ |                                      (0)-(0)                                      |
+ |                                     .-(___)-.                                     |
+ |                                      _<   >_                                      |
+ |                                      \\/   \\/                                      |
+ |                                                                                   |
+ |                                                                                   |
+ |                                                                                   |
+ |-----------------------------------------------------------------------------------|
+ """);
+}
+
+}
